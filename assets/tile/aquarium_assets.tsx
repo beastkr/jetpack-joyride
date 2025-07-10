f6bed69507d0982df3c840b2682b0890624355d0
beastkr <dang.nguyen@sun.studio>
2025-07-10 18:12:47 +0700

    Updates

---
+++ assets/tile/aquarium_assets.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="aquarium_assets" tilewidth="512" tileheight="512" tilecount="16" columns="4">
+ <image source="../atlas/aquarium_assets.png" width="2324" height="2050"/>
+</tileset>
